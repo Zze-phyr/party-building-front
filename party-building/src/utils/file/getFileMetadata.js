96e1c633fc6396852d2d39743b6314e36576a89c
wwiiinndd <2359991004@qq.com>
2025-09-14 22:35:50 +0800

    获取文件元素优化-单项数据流

--- party-building/src/utils/file/getFileMetadata.js
+++ party-building/src/utils/file/getFileMetadata.js
@@ -5,7 +5,7 @@ export const getFileMetadata = async (fileMetadataRequestParams, fileMetadata) =
   const { data: fileTemplateGetData } = await generalApi.getFileMetadata(fileMetadataRequestParams)
   if (fileTemplateGetData.code === 1) {
     if (fileTemplateGetData.data.length > 0) {
-      Object.assign(fileMetadata, fileTemplateGetData.data[0])
+      fileMetadata.value = fileTemplateGetData.data[0]
     }
   } else {
     ElMessage.error(fileTemplateGetData.msg)
